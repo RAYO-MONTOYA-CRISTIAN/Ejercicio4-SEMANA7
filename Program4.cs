9c7f7af9507dcfc4b3c70176057956eacf0f7a2d
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-10-03 23:09:00 -0500

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                for (int a = 0; a < numero; a++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
